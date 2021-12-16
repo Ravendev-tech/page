e13044e3e72a28aeed3850cf0838b0b22b8facbf
Ravendev-tech <damian.g.gutman@gmail.com>
2021-12-16 12:54:10 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -168,7 +168,6 @@
 </section>
 @endsection
 @elsedesktop
-<!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
